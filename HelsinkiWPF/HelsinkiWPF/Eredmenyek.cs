95f8da3f8b1022011cff52b09c6fd4d9f36aafcc
Erw1n0123 <kisserikervin@gmail.com>
2023-05-22 19:49:32 +0200

    fel

---
+++ HelsinkiWPF/HelsinkiWPF/Eredmenyek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelsinkiWPF
+{
+    internal class Eredmenyek
+    {
+        public Eredmenyek(string line)
+        {
+            helyezes = int.Parse(line.Split(' ')[0]);
+            embred_db = int.Parse(line.Split(' ')[1]);
+            sportag = line.Split(' ')[2];
+            versenyszam = line.Split(' ')[3];
+        }
+
+        public int helyezes { get; }
+        public int embred_db { get; }
+        public string sportag { get; }
+        public string versenyszam { get; }
+    }
+}
---
+++ HelsinkiWPF/HelsinkiWPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelsinkiWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Eredmenyek> list = new List<Eredmenyek>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StreamReader r = new StreamReader("helsinki.txt");
+            while (!r.EndOfStream)
+            {
+                list.Add(new Eredmenyek(r.ReadLine()));
+            }
+            r.Close();
+            Datagrid.ItemsSource = list;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                list.RemoveAt(Datagrid.SelectedIndex);
+                Datagrid.Items.Refresh();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static int Pont(int helyezes)
+        {
+            if (helyezes == 1)
+            {
+                return 7;
+            }
+            else if (helyezes > 1 && helyezes < 7)
+            {
+                return 7 - helyezes;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                 StreamWriter w = new StreamWriter("helsinki2.txt");
+                foreach (var item in list)
+                {
+                    w.WriteLine($"{item.helyezes} {item.embred_db} {item.sportag} {item.versenyszam} {Pont(item.helyezes)}");
+                }
+                w.Close();
+                MessageBox.Show("A mentés sikeres!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
